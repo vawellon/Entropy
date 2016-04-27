a0fbe7d529a9f80259c95159896b9aea8e983e3d
Kiran Challa <kichalla@microsoft.com>
2016-04-27 10:48:52 -0700

    Added new sample Mvc.FileUpload

---
+++ samples/Mvc.FileUpload/Controllers/RequestFormLimitsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.FileUpload.Filters;
+using Mvc.FileUpload.Models;
+
+namespace Mvc.FileUpload.Controllers
+{
+    public class RequestFormLimitsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Set the request form size limits *before* the antiforgery token validation filter is executed so that the
+        // limits are honored when the antiforgery validation filter tries to read the form.
+        [HttpPost]
+        [RequestFormSizeLimit(keyCountLimit: 3, Order = 1)]
+        [ValidateAntiForgeryToken(Order = 2)]
+        public IActionResult Create(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = new
+            {
+                Name = user.Name,
+                Age = user.Age,
+                Zipcode = user.Zipcode
+            };
+
+            return Json(result);
+        }
+    }
+}
---
+++ samples/Mvc.FileUpload/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Mvc.FileUpload
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile(source =>
+                {
+                    source.Path = "appsettings.json";
+                    source.ReloadOnChange = true;
+                })
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Angular's default header name for sending the XSRF token.
+            services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseExceptionHandler("/Home/Error");
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
